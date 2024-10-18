863c84c77b7667e7e81d156de551b4c5a8462e82
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-10-18 14:55:49 -0400

    nativewind and expo router added

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+<View className="flex-1 items-center justify-center bg-white">
+      <View >
+        <Text >Hello World</Text>
+        <Text >This is the first page of your app.</Text>
+      </View>
+    </View>
+  );
+}
